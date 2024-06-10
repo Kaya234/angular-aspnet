c07e4899632426d816feded8dbb4e0f8ac8b85cd
Karolina Savincova <karolina.savincova@gmail.com>
2024-06-10 19:46:22 +0200

    created dynamicly changing title

--- courses-page-angular-aspnet/ClientApp/src/app/app.component.ts
+++ courses-page-angular-aspnet/ClientApp/src/app/app.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+ import { Component, HostListener } from '@angular/core';
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  providers : [Title]
 })
+
+
 export class AppComponent {
-  title = 'Courses for you';
+
+
+  constructor( private title: Title) {
+    this.title.setTitle('Courses for you');
 }
+
+  @HostListener('window:focus', ['$event'])
+  onFocus(): void {
+    this.title.setTitle('Courses for you');
+  }
+
+
+  @HostListener('window:blur', ['$event'])
+  onBlur(): void {
+    this.title.setTitle('Wait, come back!');
+  }
+
+}
+
